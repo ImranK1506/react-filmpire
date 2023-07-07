fb042737fd2551b963d219278a645a22437cc6d8
Imran Khan <m_imran_khan@me.com>
2023-07-07 14:41:17 +0200

    add login and logout to alan

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
@@ -16,6 +17,13 @@ const useAlan = () => {
           } else {
             setMode('dark');
           }
+        }
+        if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+
+          window.location.href = '/';
         }
       },
     });
