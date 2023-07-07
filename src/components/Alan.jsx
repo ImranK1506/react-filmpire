50583d0f68f4bb9152e9941d5a0c2638003c5290
Imran Khan <m_imran_khan@me.com>
2023-07-07 15:34:15 +0200

    add movie genres to ai

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,16 +1,35 @@
 import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { ColorModeContext } from '../utils/ToggleColorMode';
 import { fetchToken } from '../utils';
+import { selectGenreOrCategory, searchMovie } from '../features/currentGenreOrCategory';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     alanBtn({
       key: '00152b199e239d45b1c70f624b3ef88b2e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        // Listen to genres
+        if (command === 'choseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        }
+        //  Listen to dark/light mode
         if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
@@ -18,12 +37,16 @@ const useAlan = () => {
             setMode('dark');
           }
         }
+        // Login and logout
         if (command === 'login') {
           fetchToken();
         } else if (command === 'logout') {
           localStorage.clear();
-
           window.location.href = '/';
+        }
+        // Search
+        if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
