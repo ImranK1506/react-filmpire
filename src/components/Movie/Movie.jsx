ee3dfa00bc0a0a41a484561cfb2bf05644cbb00b
Imran Khan <m_imran_khan@me.com>
2023-07-14 11:16:59 +0200

    fix poster url

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -15,7 +15,7 @@ export const Movie = ({ movie, i }) => {
             alt={movie.title}
             className={classes.image}
             src={movie.poster_path
-              ? `https:/image.tmdb.org/t/p/w500/${movie.poster_path}`
+              ? `https:/image.tmdb.org/t/p/w500${movie.poster_path}`
               : 'https://www.fillmurray.com/200/300'}
           />
         </Link>
